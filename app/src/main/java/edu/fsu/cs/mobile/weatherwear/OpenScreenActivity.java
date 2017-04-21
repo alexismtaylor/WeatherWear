e33243634a9fee25f6f4b443bf89e65ecb154fb5
Alexis Taylor <alexismtaylor2@gmail.com>
2017-04-20 20:12:22 -0400

    Added more activities

--- app/src/main/java/edu/fsu/cs/mobile/weatherwear/OpenScreenActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/weatherwear/OpenScreenActivity.java
@@ -1,13 +1,34 @@
 package edu.fsu.cs.mobile.weatherwear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OpenScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_openScreen);
+        setContentView(R.layout.activity_openscreen);
     }
+    //go to alarm activity
+    public void clickAlarm(View view)
+    {
+        Intent intent = new Intent(OpenScreenActivity.class, AlarmActivity.class);
+        startActivity(intent);
+    }
+    //go to weather activity
+    public void clickWeather(View view)
+    {
+        Intent intent = new Intent(OpenScreenActivity.class, WeatherActivity.class);
+        startActivity(intent);
+    }
+    //go to adding pictures activity
+    public void clickAdd(View view)
+    {
+        Intent intent = new Intent(OpenScreenActivity.class, AddClothingActivity.class);
+        startActivity(intent);
+    }
+
 }
