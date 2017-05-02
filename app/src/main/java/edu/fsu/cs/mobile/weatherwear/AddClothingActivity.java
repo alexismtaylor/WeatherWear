c9eb8255f3041c8a138d1d8e7d09950b1d6070ef
CIS4930group <jcfriedman95@gmail.com>
2017-05-02 13:36:23 -0400

    Removed Commented Out Code
    ^^

--- app/src/main/java/edu/fsu/cs/mobile/weatherwear/AddClothingActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/weatherwear/AddClothingActivity.java
@@ -14,6 +14,7 @@ import android.widget.Spinner;
 import android.view.View;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.AdapterView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 
@@ -58,7 +59,18 @@ public class AddClothingActivity extends AppCompatActivity implements OnItemSele
         bTake = (Button) findViewById(R.id.bTake);          //take picture button
         ivPictureTaken = (ImageView) findViewById(R.id.ivPictureTaken);
         spinner.setOnItemSelectedListener(this);
+
+        //checkEmpty();
+
     }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+       // checkEmpty();
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -122,6 +134,10 @@ public class AddClothingActivity extends AppCompatActivity implements OnItemSele
         else{
             Toast.makeText(this, "Please select a valid category.", Toast.LENGTH_SHORT).show();
         }
+
+
+      //  checkEmpty();
+
     }
 
     public void clickTake(View view){
--- app/src/main/java/edu/fsu/cs/mobile/weatherwear/OpenScreenActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/weatherwear/OpenScreenActivity.java
@@ -1,17 +1,57 @@
 package edu.fsu.cs.mobile.weatherwear;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class OpenScreenActivity extends AppCompatActivity {
+
+    Button weatherB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_openscreen);
+
+        weatherB = (Button) findViewById(R.id.bWeather);
+
+        File path = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File files[] = path.listFiles();
+
+        if(files.length==0)
+        {
+            weatherB.setVisibility(View.GONE);
+        }
+        else
+        {
+            weatherB.setVisibility(View.VISIBLE);
+        }
+
     }
+
+    protected void onResume() {
+
+        super.onResume();
+
+        File path = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File files[] = path.listFiles();
+
+        if(files.length==0)
+        {
+            weatherB.setVisibility(View.GONE);
+        }
+        else
+        {
+            weatherB.setVisibility(View.VISIBLE);
+        }
+
+    }
+
     //go to alarm activity
     public void clickAlarm(View view)
     {
